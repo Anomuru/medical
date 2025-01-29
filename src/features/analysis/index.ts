d89ca7f71a24ac131f274e347b90cc1fb6a64b79
null <raymuruvsanomuru@gmail.com>
2025-01-29 16:39:07 +0500

    add header and menu

---
+++ src/features/analysis/index.ts
@@ -0,0 +1,1 @@
+export {AnalysisPackageModal} from "./ui/analysisPackageModal";
--- src/pages/analysisPage/ui/analysisPage.tsx
+++ src/pages/analysisPage/ui/analysisPage.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import {Navigate, Outlet, Route, Routes} from 'react-router';
 
-import {AnalysisHeader} from "entities/analysis";
+import {AnalysisPackageModal} from "features/analysis";
+import {AnalysisHeader, AnalysisPackage} from "entities/analysis";
 
 import cls from "./analysisPage.module.sass";
 
 export const AnalysisPage = () => {
     return (
-        <div>
+        <div className={cls.analysis}>
             <AnalysisHeader/>
+            <Outlet/>
+            <Routes>
+                <Route path={"package"} element={<AnalysisPackageModal/>}/>
+
+                <Route index element={<Navigate to={"package"}/>}/>
+            </Routes>
         </div>
     );
 }
