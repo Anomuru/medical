9f63756bb7d37e5ee08266e6c8f72ec53ef3ea79
null <raymuruvsanomuru@gmail.com>
2025-01-29 16:04:01 +0500

    add header and menu

--- src/entities/analysis/ui/analysisHeader/analysisHeader.tsx
+++ src/entities/analysis/ui/analysisHeader/analysisHeader.tsx
@@ -1,11 +1,48 @@
-import React, {memo} from 'react';
+import {memo, useCallback} from 'react';
+import {NavLink} from "react-router";
 
 import cls from "./analysisHeader.module.sass";
 
-export const AnalysisHeader = memo(() => {
-    return (
-        <div>
+const list = [
+    {
+        label: "Paket",
+        path: "package"
+    },
+    {
+        label: "Guruh",
+        path: "group"
+    },
+    {
+        label: "Container",
+        path: "container"
+    },
+    {
+        label: "Analiz",
+        path: "analysis"
+    }
+]
 
+export const AnalysisHeader = memo(() => {
+
+    const render = useCallback(() => {
+        return list.map(item => {
+            return (
+                <NavLink
+                    className={
+                        ({isActive}) =>
+                            isActive ? `` : ``
+                    }
+                    to={item.path}
+                >
+                    {item.label}
+                </NavLink>
+            )
+        })
+    }, [])
+
+    return (
+        <div className={cls.header}>
+            {render()}
         </div>
     );
 })
