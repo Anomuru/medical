491e5762da8b59f5b37fbfa095c194ea5511ed38
DeadMonstr <deedmonstr@gmail.com>
2025-01-31 14:42:27 +0500

    add new features

--- src/shared/const/routers.ts
+++ src/shared/const/routers.ts
@@ -1,14 +1,14 @@
-export const getRegisterRoute = (id) => `register`
-export const getDashboardRoute = (id) => `dashboard`
-export const getStaffRoute = (id) => `staff`
+export const getRegisterRoute = () => `register`
+export const getDashboardRoute = () => `dashboard`
+export const getStaffRoute = () => `staff`
 export const getHospitalRegRoute = () => `hospitalReg`
 export const getPaymentRoute = () => `payment`
 export const getPlatformHomePage = () => `platformHomePage`
 export const getPricePageRoute = () => `pricePage`
 
-export const getProfileRoute = (id) => `staff/profile/${id}`
+export const getProfileRoute = (id: string) => `staff/profile/${id}`
 export const getJobListRoute = () => `jobPage`
 export const getDevicePageRoute = () => `devicePage`
-export const getDeviceProfileRoute = (id) => `devicePage/deviceProfile/:id`
+export const getDeviceProfileRoute = () => `devicePage/deviceProfile/:id`
 export const getWorkTableRoute = () => `table`
-export const getWorkerProfile = (id) => `table/workerProfile`
+export const getWorkerProfile = () => `table/workerProfile`
